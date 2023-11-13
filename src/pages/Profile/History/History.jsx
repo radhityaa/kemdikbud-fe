ebc59e784f4750ae25a43a98fe935ea3d5f21387
Rama Adhitya Setiadi <ramaadhityasetiadi002@gmail.com>
2023-11-13 10:26:35 +0700

    initial commit

--- src/pages/Profile/History/History.jsx
+++ src/pages/Profile/History/History.jsx
@@ -9,7 +9,7 @@ export default function History() {
             <ContentHeader text={'Sejarah'} />
 
             <div className='space-y-5'>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequatt.</p>
 
                 <img src={Sejarah} />
             </div>
